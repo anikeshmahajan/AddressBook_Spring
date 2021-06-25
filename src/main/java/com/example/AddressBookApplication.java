3cb852acfa4f50475cd974f5491a622f45afcb61
Anikesh <anikesh@0725@gmail.com>
2021-06-25 20:09:18 +0530

    Adding Application-dev properties file

--- src/main/java/com/example/AddressBookApplication.java
+++ src/main/java/com/example/AddressBookApplication.java
@@ -2,16 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
-		log.info("AddressBook Application is Starting");
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("AddressBook Application is Starting  in  {} Environment",
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
