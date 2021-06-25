2378db5350e981145e91ac053ef452426d95a009
Anikesh <anikesh@0725@gmail.com>
2021-06-25 19:30:30 +0530

    Adding Logs Slf4j

--- src/main/java/com/example/AddressBookApplication.java
+++ src/main/java/com/example/AddressBookApplication.java
@@ -3,12 +3,15 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookApplication.class, args);
+		log.info("AddressBook Application is Starting");
 	}
 
 }
